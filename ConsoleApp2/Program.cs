1d8bc8c3f5cf47639eeb56051e232d97663f527b
83336 <halshamlan@yahoo.com>
2024-04-14 13:44:28 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Control flow
+int currentHour = 13;
+if (currentHour >= 11 && currentHour <= 14)
+{
+    Console.WriteLine("Time for lunch!");
+}
+
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine(i);
+}
+
+/// arrays
+
+string[] daysOfWeek = { "sun", "mon", "tus", "wend", "thur"};
+Console.WriteLine("days of the week:");
+foreach (string day in daysOfWeek)
+{
+    Console.WriteLine(day);
+}
+
+// 
+int[] numbers = { 1, 6, 8, 0, 5 };
+int sum = 0;
+foreach (int number in numbers)
+{
+    sum += number;
+}
+Console.WriteLine("The sum of the numbers is:" + sum);
